7a40198083c859cb1737174dfe7770d538a6acfb
russeb85 <russeb@Eugens-MacBook.local>
2016-06-12 14:23:11 +0200

    initial-commit

---
+++ flask_server.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import pytesla_tesla
+import teslajson
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello World!"
+
+@app.route('/hello')
+def hello2():
+    return 'Hello World'
+
+pytesla_tesla.main()
+
+"""
+if __name__ == "__main__":
+    app.run(host='0.0.0.0')
+"""
